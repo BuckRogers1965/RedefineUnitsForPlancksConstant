ef08381ae823d9990f6396056107fd714233f400
James Rogers <jamesmrogers@gmail.com>
2024-11-24 13:57:46 -0500

    added more Planck constants

--- simplified_plancks_constants.py
+++ simplified_plancks_constants.py
@@ -21,6 +21,7 @@ def validate_planck_units(constants: PhysicsConstants) -> Dict[str, Tuple[float,
     """
     # Calculate Planck units using simplified formulas
     h_calc = (constants.alpha**3 * constants.beta) / constants.c
+    h_bar_calc = (constants.alpha**3 * constants.beta) /(2 * np.pi * constants.c)
     G_calc = constants.alpha**3 / constants.beta
     length_calc = (constants.alpha**3) / ((2 * np.pi)**(1/2) * constants.c**2)
     time_calc = (constants.alpha**3) / ((2 * np.pi)**(1/2) * constants.c**3)
@@ -42,16 +43,53 @@ def validate_planck_units(constants: PhysicsConstants) -> Dict[str, Tuple[float,
 
     #mag flux = (alpha^3 * beta)/ (2ec)
     #mag flux = (2ec)*(alpha^3 * beta)
+
+    # Planck angular momentum (ℏ)
+    angular_momentum_calc = h_calc / (2 * np.pi)
+    
+    # Planck energy (√(hc⁵/G))
+    energy_calc = ((constants.beta**2 * constants.c**4)/(2 * np.pi))**(1/2) 
+    
+    # Planck force (c⁴/G)
+    force_calc = constants.beta * constants.c**4 / constants.alpha**3
+    
+    # Planck power (c⁵/G)
+    power_calc = constants.beta * constants.c**5 / constants.alpha**3
+    
+    # Planck density (c⁵/(hG²))
+    density_calc = (constants.beta * constants.c**6 * 2 * np.pi)/(constants.alpha**9)
+    
+    # Planck area (length²)
+    area_calc = length_calc**2
+    
+    # Planck volume (length³)
+    volume_calc = length_calc**3
+    
+    # Planck acceleration (c²/length)
+    acceleration_calc = constants.c**2 / length_calc
+    
+    # Planck pressure (force/area = c⁷/(hbarG²))
+    pressure_calc = (constants.beta * constants.c**8 *2*np.pi) / ( constants.alpha**9)
     
     # Known values
     expected = {
         'Planck constant': 6.62607015e-34,
+        'h_bar': 1.054571817e-34,
         'Gravitational constant': 6.67430e-11, 
         'Planck Length': 1.616255e-35,
         'Planck Time': 5.391247e-44,
         'Planck Mass': 2.176434e-8,
         'Planck Charge': 1.875545e-18,
         'Planck Temperature': 1.416784e32,
+        "Planck angular momentum": 1.054571817e-34,
+        'Planck energy': 1.9561125933147e+09,
+        'Planck force': 1.2106590585939e+44,
+        'Planck power': 3.6283154625583e+52,
+        'Planck density': 5.1550074220287e+96,
+        'Planck area': 2.6120569573458e-70,
+        'Planck volume': 4.2241900034916e-105,
+        'Planck acceleration': 5.5599586942113e+51,
+        'Planck pressure': 4.63324611732735e+113,
         'Bolzmann Temperature': 1.380649e-23,
         'Epsilon_0 Temperature': 8.854187817e-12,
         'Planck Charge2': 1.875545e-18,
@@ -66,12 +104,23 @@ def validate_planck_units(constants: PhysicsConstants) -> Dict[str, Tuple[float,
     results = {}
     calcs = {
         'Planck constant': h_calc,
+        'h_bar': h_bar_calc,
         'Gravitational constant': G_calc, 
         'Planck Length': length_calc,
         'Planck Time'  : time_calc,
         'Planck Mass'  : mass_calc,
         'Planck Charge': charge_calc,
         'Planck Temperature': temp_calc,
+        "Planck angular momentum": angular_momentum_calc,
+        "Planck energy": energy_calc,
+        "Planck force": force_calc,
+        "Planck power": power_calc, 
+        "Planck density": density_calc,
+        "Planck area":  area_calc,
+        "Planck volume": volume_calc,
+        "Planck acceleration": acceleration_calc,
+        "Planck pressure": pressure_calc,
+ 
         'Bolzmann Temperature': boltzmann_calc,
         'Epsilon_0 Temperature': e0_calc,
         'Planck Charge2': charge2_calc,
